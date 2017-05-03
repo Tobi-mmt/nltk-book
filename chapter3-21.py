4e127898e0b7cbf74fd3ab835166fe7822c2b75c
Tobias Linkohr <tlinkohr.mmt-m2015@fh-salzburg.ac.at>
2017-05-03 12:19:28 +0200

    correction of exercise 21

--- chapter3-21.py
+++ chapter3-21.py
@@ -11,15 +11,18 @@ from urllib import request
 def unknown(url):
     response = request.urlopen(url)
     raw = response.read().decode('utf8')
-    raw_clean = BeautifulSoup(raw, "html.parser").get_text()
+    raw_clean = BeautifulSoup(raw, "html.parser")
+    for script in raw_clean(["script", "style"]):
+        script.decompose()  # clean from script and style tags
+    raw_clean = raw_clean.get_text()
     tokens = nltk.word_tokenize(raw_clean)
-    text = nltk.Text(tokens)
-    raw_list = [w for w in text.tokens if re.search('^\w+$', w)]
+    raw_list = [w for w in tokens if re.search('^\w+$', w)]
     known_words = nltk.corpus.words.words()
     unknown_words = [w.lower() for w in raw_list if w not in known_words]
-    return unknown_words
+    return set(unknown_words)
 
 user_iput = input('Website eingeben, z.B. haribo.de: ')
 url = "http://www." + user_iput
 
-print('Unbekannt Worte auf der Website {}: {}.'.format(user_iput, unknown(url)))
+words = unknown(url)
+print('{} Unbekannt Worte auf der Website {}: {}.'.format(len(words), user_iput, words))
