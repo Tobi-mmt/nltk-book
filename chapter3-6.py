a78f0ffcfbfd935019efd6d6bc69327ef2ccdb3a
Tobias Linkohr <tlinkohr.mmt-m2015@fh-salzburg.ac.at>
2017-05-03 11:52:43 +0200

    correction of exercise 6 & 7

--- chapter3-6.py
+++ chapter3-6.py
@@ -12,7 +12,7 @@ a = [w for w in wordlist if re.search(r'[a-zA-Z]+', w)]  # alle Wörter mit eine
 b = [w for w in wordlist if re.search(r'[A-Z][a-z]*', w)]  # alle Wörter mit Großbuchstabe am Anfang und 0 bis unendlich kleinen Buchstaben danach
 c = [w for w in wordlist if re.search(r'p[aeiou]{,2}t', w)]  # alle Wörter mit Substring, der mit p anfängt und darauf folden 0 bis 2 Umlaute hat und mit t endet
 d = [w for w in wordlist if re.search(r'\d+(\.\d+)?', w)]  # alle Zahlen mit und ohne Punkt getrennt
-e = [w for w in wordlist if re.search(r'([^aeiou][aeiou][^aeiou])*', w)]  # alle Wörter mit einem Umlaut ind er Mitte, OHNE Umlaut davor und mit variabel vielen Nicht-Umlauten danach
+e = [w for w in wordlist if re.search(r'([^aeiou][aeiou][^aeiou])*', w)]  # alle Wörter mit einem Umlaut in der Mitte, OHNE Umlaut davor und mit variabel vielen Nicht-Umlauten danach
 f = [w for w in wordlist if re.search(r'\w+|[^\w\s]+', w)]  # Wort oder nicht-white space
 
 print(f)
--- chapter3-7.py
+++ chapter3-7.py
@@ -4,7 +4,13 @@
 # 7 write regex
 # -----------------
 import nltk, re
+from nltk import word_tokenize
 
-wordlist = nltk.corpus.words.words('en')
+word_list = nltk.corpus.words.words('en')
 
-print([w for w in wordlist if re.search(r'^(a|the|an)$', w)])
+print([w for w in word_list if re.search(r'^(a|the|an)$', w)])
+
+
+digits = "The student didn't know how to calculate 3+4 or 45*2+12 or 3+3+3 or 444+5*9000 or 4*4."
+word_list = word_tokenize(digits)
+print([w for w in word_list if re.search(r'^(\d|\*|\+)+$', w)])
