44fed8c33e49fbfcfdf343fe51f379167462653a
Tobias Linkohr <tlinkohr.mmt-m2015@fh-salzburg.ac.at>
2017-05-05 00:54:47 +0200

    added list of exercises

---
+++ chapter_4/exercise_2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 2, 10, 13, 14, 16, 17, 18, 19, 21, 23, 26, 31, 32, 35
+# extra - 27: https://pdfs.semanticscholar.org/3973/ff27eb173412ce532c8684b950f4cd9b0dc8.pdf
+
+##################
+# 2 - three operations that can be performed on both tuples and lists
+##################
