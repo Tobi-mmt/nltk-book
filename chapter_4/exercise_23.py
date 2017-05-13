76978dbf1ae740cfd7fdd7e6b1844edfb4970e49
Tobias Linkohr <tlinkohr.mmt-m2015@fh-salzburg.ac.at>
2017-05-13 13:51:02 +0200

    chapter 4 - exercise 23

---
+++ chapter_4/exercise_23.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+# 2, 10, 13, 14, 16, 17, 18, 19, 21, 23, 26, 31, 32, 35
+# extra - 27: https://pdfs.semanticscholar.org/3973/ff27eb173412ce532c8684b950f4cd9b0dc8.pdf
+
+##################
+# 23 - Write a recursive function lookup(trie, key) that looks up a key in a trie, and returns the value it finds.
+# Extend the function to return a word when it is uniquely determined by its prefix
+# (e.g. vanguard is the only word that starts with vang-, so lookup(trie, 'vang') should return the same thing
+# as lookup(trie, 'vanguard')).
+##################
+import pprint
+
+
+def insert(trie, key, value):
+    if key:
+        first, rest = key[0], key[1:]
+        if first not in trie:
+            trie[first] = {}
+        insert(trie[first], rest, value)
+    else:
+        trie['value'] = value
+
+
+def lookup(trie, key):
+    if key:
+        first, rest = key[0], key[1:]
+        if first not in trie:
+            return 'no result'
+        return lookup(trie[first], rest)
+    else:
+        try:
+            return trie['value']
+        except:
+            return lookup(trie, next(iter(trie)))
+
+trie = {}
+insert(trie, 'chat', 'cat')
+insert(trie, 'hello', 'world')
+insert(trie, 'head', 'cap')
+insert(trie, 'tree', 'leaf')
+insert(trie, 'red', 'blue')
+
+trie = dict(trie)
+search_key = input('enter search key: ')
+print('Result for "{}" is "{}"'.format(search_key, lookup(trie, search_key)))
