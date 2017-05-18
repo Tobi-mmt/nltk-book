f26c0374973d8c24c26a6179af28c5b34f4d5ed2
Tobias Linkohr <tlinkohr.mmt-m2015@fh-salzburg.ac.at>
2017-05-18 15:06:11 +0200

    added list of exercises

---
+++ chapter_5/exercise_11.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# 11, 13, 15, 17, 27, 34, 36, 39, 40, 43
+
+##################
+# 11 - Learn about the affix tagger (type help(nltk.AffixTagger)). Train an affix tagger and run it on some new text.
+# Experiment with different settings for the affix length and the minimum word length. Discuss your findings.
+##################
